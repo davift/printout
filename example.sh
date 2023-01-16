3cd4ce63e0a20cc93cc511cc298987dd6906660d
davift <73488542+davift@users.noreply.github.com>
2023-01-15 19:52:19 -0500

    Add files via upload

--- example.sh
+++ example.sh
@@ -2,17 +2,25 @@
 
 source printout
 
-printout red "This is a red message" bold underline bg_green
-printout green "This is a green message" italic bg_purple
-printout purple "This is a purple message" bg_yellow
-printout grey "This is a black message" bg_white
-printout cyan "This is a cyan message" bold underline
-printout green "This is a green message" italic
-printout purple "This is a purple message" 
-printout yellow "This is an yellow message" 
-printout black "This is a black message"
-printout cyan "This is a cyan message"
-printout white "This is a white message"
-printout pink "This is a pink message"
+printout "This is a red message" red 
+printout "This is a green bold message" green bold
+printout "This is an yellow bold underlined message" yellow bold underline
+printout "This is a purple message" purple 
+printout "This is a cyan italic message" cyan italic 
+printout "This is a black bold italic message" black bold italic
+printout "This is a white underline italic bold message" white underline italic bold
+printout "This is a grey message" grey
+printout "This is a pink message" pink 
+printout "This is a message"
+printout "This is a green bold on grey message" yellow bold bg_grey
+printout "This is a black on pink message" black bg_pink
+printout "This is a grey on white message" grey bg_white
+printout "This is a white on black message" white bg_black
+printout "This is a black on cyan message" cyan bg_cyan
+printout "This is a gray on purple message" grey bg_purple
+printout "This is a black on yellow message" black bg_yellow
+printout "This is a black on green message" black bg_green
+printout "This is a white on red message" white bg_red
+printout "This is a transparent message" transparent
 
 exit 0
